5d5859a631a57d80f33d1cbb49339168b136fbea
Felucca24 <felucca24@gmail.com>
2020-06-06 19:03:54 +0400

    Удалены объёмные листья. Теперь это отдельный ресурспак.

--- sources/make.py
+++ sources/make.py
@@ -1,50 +1,19 @@
 import os
 import json
 
+# Задача скрипта - генерировать конечные конфигурации ресурспака, а
+# именно - версии:
+# - Для использования с шейдерами
+# - Для использования с шейдерами
+# - Листья отдельным ресурспаком
+
+
 dirpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PACK_MCMETA_PATH = os.path.join(dirpath, "pack.mcmeta")
-
-ver = input("Resourcepack version > ")
-
-SHADERS = "§e§o"
-NO_SHADERS = "§a§o"
-
-MC_1_14 = "§e§l1.14 - §e1.15"
-MC_1_15 = "§e1.14 - §e§l1.15§e"
-
-NO_SHADERS_EXCLUDE = ["_n.png", "_s.png"]
-NO_SHADERS_ANALOGS_PATH = os.path.join(dirpath, "sources",
-                                       "no_shaders_analogs")
-
 configurations = {
-    "1.14-no-shaders": {
-        "ver": NO_SHADERS + ver,
-        "mc_ver": MC_1_14
-    },
-    "1.15-no-shaders": {
-        "ver": NO_SHADERS + ver,
-        "mc_ver": MC_1_15
-    },
-    "1.14": {
-        "ver": SHADERS + ver,
-        "mc_ver": MC_1_14
-    },
-    "1.15": {
-        "ver": SHADERS + ver,
-        "mc_ver": MC_1_15
-    },
-}
-
-desc = "§6§oOutbreak §e§o{ver}§7 by §6§oFeelinVoids_\n§e{mc_ver} §7• §cOptiFine required"
-
-pack_mcmeta = {
-   "pack": {
-      "pack_format": 4,
-      "description": desc
-   }
+    "leaves-no-shaders"
 }
 
 
-for conf in configurations:
-    with open(PACK_MCMETA_PATH, "w", encoding="utf-8") as f:
+
+
