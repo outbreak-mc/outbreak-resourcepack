73c75973d8b095f0104f9cf8cfbaf23a9b0f76ff
Felucca24 <felucca24@gmail.com>
2020-07-05 22:18:27 +0400

    Переделал каменные кирпичи

--- assets/minecraft/textures/block/parallax_generator/parallax_maker.py
+++ assets/minecraft/textures/block/parallax_generator/parallax_maker.py
@@ -1,15 +1,43 @@
 import os
 import shutil
+import sys
+import traceback
 from typing import Optional
 
 from PIL import Image
 
-# Скрипт ищет в папке карты нормалей (файлы с окончанием _n) и 
-# генерирует для них параллакс путём изменения альфы в зависимости
-# от синего канала. Старые файлы бэкапит в ./.normalmaps_backup
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+backup_dir = os.path.join(dirpath, ".normalmaps_backup")
 
 
-def generate_parallax(img):
+def make_backup(img_path):
+    """ Делает бэкап картинки в папку ./.normalmaps_backup """
+    if not os.path.exists(backup_dir):
+        print("Making dir", backup_dir)
+        os.mkdir(backup_dir)
+    img_name = os.path.basename(img_path)
+    backup_path = os.path.join(backup_dir, img_name)
+    shutil.copy(img_path, backup_path)
+    return backup_path
+
+
+def generate_parallax(img_path: str):
+    """ Генерирует параллакс (альфа-канал) для карты нормалей
+        по пути img_path на основе синего канала.
+
+        Картинка перезаписывается, но перед этим делается бэкап
+        в ./.normalmaps_backup """
+
+    # Проверяем, картинка ли это вообще
+    if not ".png" in os.path.basename(img_path):
+        raise ValueError("Invalid path", img_path)
+
+    # Делаем бэкап
+    make_backup(img_path)
+
+    # Открываем картинку и делаем нужные операции
+    img = Image.open(img_path)
     new_data = []
     img = img.convert("RGBA")
     for px in list(img.getdata()):
@@ -22,27 +50,19 @@ def generate_parallax(img):
         new_data.append(tuple(px))
 
     img.putdata(new_data)
-    return img
+    img.save(img_path)
+    print("Generated parallax for", os.path.basename(img_path))
 
 
-dirpath = os.path.dirname(os.path.abspath(__file__))
-backup_dir = os.path.join(dirpath, ".normalmaps_backup")
-
-if not os.path.exists(backup_dir):
-    print("Making dir", backup_dir)
-    os.mkdir(backup_dir)
-
-for fn in os.listdir(dirpath):
-    if fn.endswith("_n.png"):
-        imgpath = os.path.join(dirpath, fn)
-        print("Generating parallax for", imgpath)
-        img = Image.open(imgpath)
-        img.convert("RGBA")
-        img = generate_parallax(img)
-        if img is not None:
-            shutil.copy(imgpath, os.path.join(backup_dir, fn))
-            print("Creating a backup of", fn)
-            img.save(os.path.join(dirpath, fn))
-            print("Generated parallax for", fn)
+try:
+    paths = sys.argv[1:]
+    if len(paths) > 0:
+        for p in paths:
+            generate_parallax(p)
+    else:
+        while True:
+            generate_parallax(input("> "))
+except:
+    traceback.print_exc()
 
 input("done")
