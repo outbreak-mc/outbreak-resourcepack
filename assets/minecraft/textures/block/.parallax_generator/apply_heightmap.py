4d0eb1efc305e8244fd58ed2b1172a92395fa3c7
Felucca24 <felucca24@gmail.com>
2020-07-26 00:03:38 +0400

    Исправлены чёрные рельсы. Улучшен параллакс пурпурных блоков.

--- assets/minecraft/textures/block/.parallax_generator/apply_heightmap.py
+++ assets/minecraft/textures/block/.parallax_generator/apply_heightmap.py
@@ -1,21 +1,12 @@
 import sys
 import os
+import traceback
 from PIL import Image
 
-# В скрипт через первый аргумент (drag&drop файла на скрипт в Windows)
-# передаётся путь к файлу с _n в названии. Скрипт ищет файл с похожим
-# названием, но оканчивающийся на _h. Файл, оканчивающийся на _h должен
-# быть чёрно-белой картой высот.
-# Скрипт делает картинке с _n в названии прозрачность в соответствии
-# с картой высот.
 
-
-def apply_heightmap(nm_path: str, hm_path: str):
+def apply_heightmap(nm_path: str, hm_path: str, strength: float,
+                    out_path: str):
     new_data = []
-    print()
-    print()
-    print()
-    print(hm_path)
 
     nm_img = Image.open(nm_path)
     hm_img = Image.open(hm_path)
@@ -31,35 +22,34 @@ def apply_heightmap(nm_path: str, hm_path: str):
     for num, px in enumerate(list(nm_data)):
         px = list(px)
         hm_px = hm_data[num]
-        # print(hm_avg)
-        # print(hm_px)
+
         avg = int((hm_px[0]+hm_px[1]+hm_px[2])/3)
 
-        px = [px[0], px[1], px[2], avg]
+        diff = 255-avg
+
+        px = [px[0], px[1], px[2], int(255-(diff*strength))]
         new_data.append(tuple(px))
 
     nm_img.putdata(new_data)
-    # input()
-    return nm_img
+
+    filename = os.path.basename(nm_path)
+
+    if out_path == "":
+        out_path = os.path.join(".", filename)
+    elif os.path.isdir(out_path):
+        out_path = os.path.join(out_path, filename)
+
+    nm_img.save(out_path)
 
 
 
-for arg in sys.argv:
-    if arg.endswith("_n.png"):
-        nm_path = arg
-    elif arg.endswith("_h.png"):
-        hm_path = arg
-
-if "" in (nm_path, hm_path):
-    raise FileNotFoundError(nm, hm)
-
-dirpath = os.path.dirname(os.path.abspath(__file__))
-
-out_dir = os.path.dirname(nm_path)
-out_name = os.path.basename(nm_path)
-
-out_img = apply_heightmap(nm_path, hm_path)
-
-out_img.save(os.path.join(out_dir, "out_"+out_name))
-
-input("done")
+try:
+    apply_heightmap(
+        input("normal map path > "),
+        input("grayscale height map path > "),
+        float(input("strength (0.0 - 1.0) > ")),
+        input("out path > ")
+    )
+except:
+    traceback.print_exc()
+    input()
